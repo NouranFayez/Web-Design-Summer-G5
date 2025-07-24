93b63445cfd437c72c104e32b9b2e24f1eabb51e
NouranFayez <nouran_ahmed269@hotmail.com>
2025-07-24 13:38:43 +0300

    session 15

--- session15/js/script.js
+++ session15/js/script.js
@@ -44,26 +44,90 @@
 
 // jquery ==>  write less do more
 
-$(document).ready(function(){
-    // document.querySelector('.owl-carousel')
-  $(".owl-carousel").owlCarousel({
-    items : 2 ,
-    margin: 30,
-    loop:true,
-    nav : true,
-    responsive: {
-         0:{
-            items:1
-        },
-        800:{
-            items:2
-        }
-    },
-    navText : [ '<i class="fa-solid fa-arrow-left text-primary"></i>' , '<i class="fa-solid fa-arrow-right"></i>' ]
-  });
-});
+if (document.querySelector('.owl-carousel')) {
+    $(document).ready(function () {
+        // document.querySelector('.owl-carousel')
+        $(".owl-carousel").owlCarousel({
+            items: 2,
+            margin: 30,
+            loop: true,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                800: {
+                    items: 2
+                }
+            },
+            navText: ['<i class="fa-solid fa-arrow-left text-primary"></i>', '<i class="fa-solid fa-arrow-right"></i>']
+        });
+    });
+}
 
 
 let design = document.querySelectorAll('[data-filter=design]')
+let marketing = document.querySelectorAll('[data-filter=marketing]')
+let filtersTab = document.getElementsByClassName('filter-cards')
 console.log(design)
+
+
+let myForm = document.getElementById('myForm')
+
+let handleError = (element, msg = "") => {
+    element.nextElementSibling.innerText = msg
+}
+
+let nameValidation = (element) => {
+    let inputValue = element.value.trim()
+    if (inputValue.length < 3) handleError(element, "enter at least 3 character")
+    else handleError(element)
+
+}
+
+let emailValidation = (element) => {
+    // nouran_ahmed269@hotmail.com
+    // $%*_-&! ==> special characters
+    // console.log(element.value)
+    // let regExp = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    // let regExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/;
+
+    // https://www.mailercheck.com/articles/email-validation-javascript
+    let regExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    console.log(regExp.test(element.value));
+        (!regExp.test(element.value)) ?
+        handleError(element, "please enter a valid email")
+        : handleError(element)
+}
+
+let passwordValidation =(element)=>{
+    // let d = new Date()
+    // var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+    // https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
+    var strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+    console.log(strongRegex.test(element.value));
+    (!strongRegex.test(element.value))?
+     handleError(element , 'please enter at least 1 lowercase letter , 1 uppercase letter .......')
+    : handleError(element)
+}
+
+
+
+myForm.addEventListener('input', function (e) {
+    // console.log(e)
+    // console.log(e.target.id)
+    if (e.target.id == "userName") {
+        // console.log(e.target.value.length)
+        nameValidation(e.target)
+    }
+    else if (e.target.id == 'userEmail') {
+        emailValidation(e.target)
+    }
+    else if (e.target.id == 'userPassword') {
+        passwordValidation(e.target)
+    }
+})
+
+
 
